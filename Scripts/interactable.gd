9e42ec07de5c398aa1f78f5d8d95008aed71612e
DBrownDHen <dbrownki@udel.edu>
2026-04-10 14:45:41 -0400

    CollisionShape2D Name Change

--- Scripts/interactable.gd
+++ Scripts/interactable.gd
@@ -8,7 +8,6 @@ class_name Interactable extends Area2D
 func interact():
 	push_warning("This interactable doesn't have Interact() code yet.")
 
-
 # interactable types
 # Opening a door (with key/collectible)
 # Collecting a key (pick up object)
