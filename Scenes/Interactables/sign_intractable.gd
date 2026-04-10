240c58474679abcf47442312b958d1bdce4482e3
DBrownDHen <dbrownki@udel.edu>
2026-04-10 14:27:57 -0400

    Creates Interactor/Interactable Pattern

---
+++ Scenes/Interactables/sign_intractable.gd
@@ -0,0 +1,4 @@
+extends Interactable
+
+func interact():
+	print("This is a sign")
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,10 +4,17 @@ var direction: Vector2
 @export var speed: int = 100
 @onready var player_animation: AnimatedSprite2D = $PlayerAnimation
 
+@export var nearby_interactables = []
+
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("move_left","move_right", "move_up", "move_down")
 	velocity = direction * speed
 	animations()
+	
+	if Input.is_action_just_pressed("interact"):
+		if nearby_interactables:
+			nearby_interactables.back().interact()
+		
 	move_and_slide()
 
 func animations():
@@ -23,3 +30,12 @@ func animations():
 	if direction == Vector2 (0,1):
 		player_animation.play("front_flying")
 		player_animation.flip_h = false
+
+
+func _on_interaction_detector_area_entered(area: Area2D) -> void:
+	print("Interactable Detected")
+	nearby_interactables.append(area)
+
+func _on_interaction_detector_area_exited(area: Area2D) -> void:
+	print("Interactable Removed")
+	nearby_interactables.erase(area)
