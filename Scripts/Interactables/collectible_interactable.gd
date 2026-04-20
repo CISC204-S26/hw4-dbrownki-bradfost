6ea821b7613ff213eaf6b0ecaa5fb20a786815e5
DBrownDHen <dbrownki@udel.edu>
2026-04-20 14:19:50 -0400

    Commit changes for Bradley

--- Scripts/Interactables/collectible_interactable.gd
+++ Scripts/Interactables/collectible_interactable.gd
@@ -4,5 +4,6 @@ signal collectibleCollected
 
 func interact():
 	collectibleCollected.emit()
+	Global.orbsCollected += 1
 	print("Hey you collected an orb and now have " + str(Global.orbsCollected) + " orbs")
 	queue_free()
